89b0442aa35000f622d8fb2868a6a881ec12a731
Claudio Lanconelli <claudiolanconelli@gmail.com>
2008-01-30 17:23:00 +0000

    - Add probe and fuse/lock read/write with NOTSUPPORTED case

--- at89sxx.cpp
+++ at89sxx.cpp
@@ -33,6 +33,8 @@
 #include "errcode.h"
 #include "eeptypes.h"
 
+#include "e2awinfo.h"
+
 #undef	BANK_SIZE
 #define	BANK_SIZE	1
 
@@ -45,22 +47,46 @@ At89sxx::At89sxx(e2AppWinInfo *wininfo, BusIO *busp)
 
 int At89sxx::SecurityRead(DWORD &bits)
 {
-	return OK;
+	int rv = Probe();		//No size probe needed, just probe for presence
+
+	if (rv != DEVICE_UNKNOWN)
+	{
+		rv = GetBus()->ReadLockBits(bits, GetAWInfo()->GetEEPType());
+	}
+	return rv;
 }
 
 int At89sxx::SecurityWrite(DWORD bits)
 {
-	return GetBus()->WriteLockBits(bits);
+	int rv = Probe();		//No size probe needed, just probe for presence
+
+	if (rv != DEVICE_UNKNOWN)
+	{
+		rv = GetBus()->WriteLockBits(bits, GetAWInfo()->GetEEPType());
+	}
+	return rv;
 }
 
-int At89sxx::FusesRead(DWORD &value)
+int At89sxx::FusesRead(DWORD &bits)
 {
-	return OK;
+	int rv = Probe();		//No size probe needed, just probe for presence
+
+	if (rv != DEVICE_UNKNOWN)
+	{
+		rv = GetBus()->ReadFuseBits(bits, GetAWInfo()->GetEEPType());
+	}
+	return rv;
 }
 
-int At89sxx::FusesWrite(DWORD value)
+int At89sxx::FusesWrite(DWORD bits)
 {
-	return OK;
+	int rv = Probe();		//No size probe needed, just probe for presence
+
+	if (rv != DEVICE_UNKNOWN)
+	{
+		rv = GetBus()->WriteFuseBits(bits, GetAWInfo()->GetEEPType());
+	}
+	return rv;
 }
 
 int At89sxx::QueryType(long &type)
@@ -74,6 +100,12 @@ int At89sxx::QueryType(long &type)
 	UserDebug2(UserApp2, "At89sxx::ParseID(30) *** 0x%02X - 0x%02X\n", code[0], code[1]);
 
 	type = 0;
+	if (code[0] == 0x1E && code[1] == 0x72)
+	{
+		type = AT89S8252;
+		rv = OK;
+	}
+	else
 	if (code[0] == 0x1E && code[1] == 0x73)
 	{
 		type = AT89S8253;
@@ -107,12 +139,49 @@ int At89sxx::QueryType(long &type)
 
 int At89sxx::Probe(int probe_size)
 {
-	return GetSize();
+	int rv = OK;
+
+	UserDebug1(UserApp2, "At89sxx::Probe(%d) IN\n", probe_size);
+
+	if (THEAPP->GetIgnoreFlag())
+	{
+		rv = GetSize();
+	}
+	else
+	{
+		switch (GetAWInfo()->GetEEPType())
+		{
+		case AT89S51: case AT89S52:
+		case AT89S8253:
+			{
+				long type;
+				rv = QueryType(type);
+				int subtype = GetE2PSubType(type);
+
+				if (rv == OK)
+				{
+					if ( GetAWInfo()->GetEEPSubType() == subtype )
+						rv = GetSize();
+					else
+						rv = DEVICE_BADTYPE;
+				}
+			}
+			break;
+		default:
+			rv = GetSize();
+			break;
+		}
+	}
+	UserDebug1(UserApp2, "At89sxx::Probe() = %d **  OUT\n", rv);
+
+	rv = GetSize();
+
+	return rv;
 }
 
 int At89sxx::Read(int probe, int type)
 {
-	int rv = Probe( probe || GetNoOfBank() == 0 );
+	int rv = Probe();
 
 	if (rv > 0)
 	{
@@ -125,6 +194,16 @@ int At89sxx::Read(int probe, int type)
 				if (type & DATA_TYPE)
 					rv = ReadData();
 			}
+			if (rv > 0 && (type & CONFIG_TYPE))
+			{
+				// read the fuses
+				DWORD f = 0;
+				if ( GetBus()->ReadFuseBits(f, GetAWInfo()->GetEEPType()) == OK )
+					GetAWInfo()->SetFuseBits(f);
+				f = 0;
+				if ( GetBus()->ReadLockBits(f, GetAWInfo()->GetEEPType()) == OK )
+					GetAWInfo()->SetLockBits(f);
+			}
 		}
 	}
 
@@ -133,7 +212,7 @@ int At89sxx::Read(int probe, int type)
 
 int At89sxx::Write(int probe, int type)
 {
-	int rv = Probe( probe || GetNoOfBank() == 0 );
+	int rv = Probe();
 
 	if (rv > 0)
 	{
@@ -146,9 +225,18 @@ int At89sxx::Write(int probe, int type)
 				if (type & DATA_TYPE)
 					rv = WriteData();
 			}
+			if (rv > 0 && (type & CONFIG_TYPE))
+			{
+				//write the fuses
+				DWORD f = GetAWInfo()->GetFuseBits();
+				GetBus()->WriteFuseBits(f, GetAWInfo()->GetEEPType());
+
+				//write the locks
+				f = GetAWInfo()->GetLockBits();
+				GetBus()->WriteLockBits(f,  GetAWInfo()->GetEEPType());
+			}
 		}
 	}
-
 	return rv;
 }
 
@@ -165,17 +253,32 @@ int At89sxx::Verify(int type)
 		if (localbuf == 0)
 			return OUTOFMEMORY;
 
-		int v_data = OK, v_prog = OK;
+		int v_data = OK, v_prog = OK, v_config = OK;
 
 		if (type & PROG_TYPE)
 			v_prog = VerifyProg(localbuf);
 		if (type & DATA_TYPE)
 			v_data = VerifyData(localbuf);
+		if (type & CONFIG_TYPE)
+		{
+			DWORD fval, lval;
+			int fret, lret;
 
-		rval = (v_prog == OK && v_data == OK) ? 1 : 0;
+			// read the fuses & locks
+			fret = GetBus()->ReadFuseBits(fval, GetAWInfo()->GetEEPType());
+			lret = GetBus()->ReadLockBits(lval, GetAWInfo()->GetEEPType());
+
+			if ( (lret == NOTSUPPORTED || GetAWInfo()->GetLockBits() == lval)
+				&& (fret == NOTSUPPORTED || GetAWInfo()->GetFuseBits() == fval) )
+			{
+				v_config = OK;
+			}
+			else
+				v_config = 1;
+		}
+		rval = (v_prog == OK && v_data == OK && v_config == OK) ? 1 : 0;
 
 		delete localbuf;
 	}
-
 	return rval;
 }
