2cf88ef273ec2a44333b070c60dba399b0ab8b4f
Claudio Lanconelli <claudiolanconelli@gmail.com>
2009-11-16 22:28:34 +0000

    Fixed build for MSVC8

--- types.h
+++ types.h
@@ -9,8 +9,12 @@
 #ifndef _TYPES_H
 #define _TYPES_H
 
-#if defined(_MSC_VER) && _MSC_VER == 1200
-//MSVC Version 6 doesn't define stdint.h
+#ifdef	__GNUC__
+
+#include <stdint.h>
+
+#elif defined(_MSC_VER)	// && _MSC_VER == 1200
+//MSVC doesn't define stdint.h
 
 typedef unsigned long uint32_t;
 typedef unsigned short uint16_t;
@@ -20,31 +24,16 @@ typedef signed long int32_t;
 typedef signed short int16_t;
 typedef signed char int8_t;
 
-#else
-
-#include <stdint.h>
-
 #endif
 
 #ifndef	HIDDEN
 #define	HIDDEN	static
 #endif
 
-/* Amiga conventions */
-typedef uint32_t	ULONG;		/* unsigned 32-bit quantity */
-typedef uint16_t	UWORD;		/* unsigned 16-bit quantity */
-typedef uint8_t		UBYTE;		/* unsigned  8-bit quantity  */
-//typedef unsigned char	*STRPTR;	/* string pointer           */
-
 /* Windows conventions */
 typedef	uint32_t	DWORD;		/* unsigned 32-bit quantity */
-typedef	uint16_t	WORD;		/* unsigned 16-bit quantity */
-typedef	uint8_t		BYTE;		/* unsigned  8-bit quantity */
-
-/* Other conventions */
-typedef uint32_t	WORD32;		/* unsigned 32-bit quantity */
-typedef uint16_t	WORD16;		/* unsigned 16-bit quantity */
-typedef uint8_t		WORD8;		/* unsigned  8-bit quantity */
+//typedef	uint16_t	WORD;		/* unsigned 16-bit quantity */
+//typedef	uint8_t		BYTE;		/* unsigned  8-bit quantity */
 
 #ifdef	__GNUC__
 #define	PACK	__attribute__ ((packed))
