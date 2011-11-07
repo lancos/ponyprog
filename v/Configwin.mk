0272acb3dd360a2e4c81504510ff6bcc4e404ad2
Claudio Lanconelli <claudiolanconelli@gmail.com>
2011-11-07 15:11:09 +0000

    added win config.mk

---
+++ v/Configwin.mk
@@ -0,0 +1,17 @@
+#---------------------------------------------------------------------
+# Select Debug or no Debug
+#---------------------------------------------------------------------
+#DEBUG	= yes
+DEBUG	= no
+
+TOOLCHAINPREFIX = i586-mingw32msvc-
+CC	= $(TOOLCHAINPREFIX)gcc
+CXX	= $(TOOLCHAINPREFIX)g++
+RANLIB	= $(TOOLCHAINPREFIX)ranlib
+
+PONYVER = 2.08b
+
+#---------------------------------------------------------------------
+# Define filename extensions that are targeted by cleanup's
+#---------------------------------------------------------------------
+CLEANEXTS= *.bak *~ *.tmp core *.o
