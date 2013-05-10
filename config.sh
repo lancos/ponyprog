07167662803f9004c387d1c189a782ac80da7ebf
Claudio Lanconelli <claudiolanconelli@gmail.com>
2013-05-10 09:13:51 +0000

    add config script

---
+++ config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#$Id$
+
+CDHERE=`pwd`
+
+if [ ! -d "v" ]; then
+	echo "Invalid path. You must run $0 from PonyProg source dir."
+	exit 1
+fi
+
+if [ -z "$1" ]; then
+	echo "No parameter: default INSTALL_PREFIX (/usr/local)"
+else
+	echo "INSTALL_PREFIX=$1"
+	sed -i "s@INSTALL_PREFIX = /usr/local@$1@" v/Config.mk
+fi
+
+sed -i "s@HOMEV = your_ponyprog_path/software/v@HOMEV = $CDHERE/v@" v/Config.mk
