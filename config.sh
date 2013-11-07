bf2ff654611b0d81bc0ee5a815c6cb867db2bfc1
Claudio Lanconelli <claudiolanconelli@gmail.com>
2013-11-07 09:49:02 +0000

    Add check to run config.sh a second time

--- config.sh
+++ config.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 #$Id$
 
+set -o errexit
+
 CDHERE=`pwd`
+VCONFIG="v/Config.mk"
 
 if [ ! -d "v" ]; then
 	echo "Invalid path. You must run $0 from PonyProg source dir."
 	exit 1
 fi
 
-if [ ! -f "v/Config.mk" ]; then
-	echo "File v/Config.mk not found. You must run $0 from PonyProg source dir."
+if [ ! -f "$VCONFIG" ]; then
+	echo "File $VCONFIG not found. You must run $0 from PonyProg source dir."
 	exit 1
 fi
+
+if ! grep "HOMEV = your_ponyprog_path" $VCONFIG > /dev/null; then
+	if [ -f "$VCONFIG.ori" ]; then
+		echo "Already configured, restore original"
+		mv $VCONFIG.ori $VCONFIG
+	fi
+fi
+
+if ! grep "HOMEV = your_ponyprog_path" $VCONFIG > /dev/null; then
+	echo "Already configured, exiting"
+	exit 1
+fi
+
+sed -i.ori "s@HOMEV = your_ponyprog_path/software/v@HOMEV = $CDHERE/v@" $VCONFIG
 
 if [ -z "$1" ]; then
 	echo "No parameter: default INSTALL_PREFIX (/usr/local)"
 else
 	echo "INSTALL_PREFIX=$1"
-	sed -i "s@INSTALL_PREFIX = /usr/local@INSTALL_PREFIX = $1@" v/Config.mk
+	sed -i "s@INSTALL_PREFIX = /usr/local@INSTALL_PREFIX = $1@" $VCONFIG
 fi
 
-sed -i "s@HOMEV = your_ponyprog_path/software/v@HOMEV = $CDHERE/v@" v/Config.mk
