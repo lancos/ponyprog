e7178f9320de3a5a641f884479ba6e2b618241b6
Claudio Lanconelli <claudiolanconelli@gmail.com>
2017-04-28 11:16:32 +0200

    customized ponyprog style

--- astyle.sh
+++ astyle.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
-CPPCHCK=`type -p astyle`
+CMD=`type -p astyle`
 
-if [ ! -f "$CPPCHCK" ]; then
+if [ ! -x "$CMD" ]; then
    # not found exit
    echo "please install astyle and restart this script"
-   exit 0
+   exit 1
+fi
+
+ASTYLE_VERSION="Artistic Style Version 3."
+
+if [[ "`$CMD --version 2>&1`" != ${ASTYLE_VERSION}* ]]; then
+        echo "Requested ${ASTYLE_VERSION}"
+	exit 2
 fi
 
 set -e
  
 export ARTISTIC_STYLE_OPTIONS="\
 --mode=c \
---style=ansi \
---indent=tab=8\
---indent-classes \
---indent-cases \
---indent-col1-comments \
---indent-preprocessor \
---break-blocks \
+--style=allman \
+--indent=force-tab=4 \
+--indent-modifiers \
+--indent-switches \
+--indent-preproc-define \
 --pad-oper \
---add-brackets \
---convert-tabs \
+--pad-comma \
+--unpad-paren \
+--pad-header \
+--align-pointer=name \
+--align-reference=name \
+--add-braces \
 --formatted \
 --lineend=linux"
 
-for i in $(find . -type f \( -name "*.cpp" -or -name "*.c"  -or -name "*.h" \));
-do
-    astyle $ARTISTIC_STYLE_OPTIONS "$i";
-done
+$CMD $ARTISTIC_STYLE_OPTIONS --suffix=none --recursive  "SrcPony/*.cpp" "SrcPony/*.h"
 
-for i in $(find . -type f \( -name "*.orig" -or -name "*~" -or -name "moc_*" \));
-do
-    rm -if "$i";
-done
+#for i in $(find . -type f \( -name "*.cpp" -or -name "*.c"  -or -name "*.h" \));
+#do
+#    $CMD $ARTISTIC_STYLE_OPTIONS "$i";
+#done
+
+#for i in $(find . -type f \( -name "*.orig" -or -name "*~" -or -name "moc_*" \));
+#do
+#    rm -if "$i";
+#done
