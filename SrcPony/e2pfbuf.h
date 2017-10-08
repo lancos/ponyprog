ebf0d9fedc4e522489e884451cde66abb1bf34ab
Eduard Kalinowski <eduard_kalinowski@yahoo.de>
2017-10-08 22:28:35 +0200

    rename COPYING to LICENSE
    (cherry picked from commit 0688aafc506e2f3f98faa76d57248d07bef5c41b)

--- SrcPony/e2pfbuf.h
+++ SrcPony/e2pfbuf.h
@@ -19,10 +19,9 @@
 // General Public License for more details.                                //
 //                                                                         //
 // You should have received a copy of the GNU  General Public License      //
-// along with this program (see COPYING);     if not, write to the         //
+// along with this program (see LICENSE);     if not, write to the         //
 // Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. //
 //                                                                         //
-//-------------------------------------------------------------------------//
 //=========================================================================//
 
 #ifndef _E2PFBUF_H
