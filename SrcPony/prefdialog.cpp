4b9372929b90461ef0b8b6c214c7b3379111139a
lancos <claudiolanconelli@gmail.com>
2019-08-25 12:38:20 +0200

    add Timings tab to Preferences dialog

--- SrcPony/prefdialog.cpp
+++ SrcPony/prefdialog.cpp
@@ -64,6 +64,35 @@ PrefDialog::PrefDialog(QWidget *parent, const QString title) :
 	ui->cbxBusSpeedPIC->setCurrentIndex(E2Profile::GetPICSpeed());
 	ui->cbxBusSpeedSDE->setCurrentIndex(E2Profile::GetSDESpeed());
 	ui->cbxBusSpeedMicroWire->setCurrentIndex(E2Profile::GetMicroWireSpeed());
+
+	ui->label_1->setText("Power Up Delay");
+	ui->label_2->setText("SPI Reset pulse");
+	ui->label_3->setText("SPI Delay After Reset");
+	ui->label_4->setText("AT89 Delay After Reset");
+	ui->label_5->setText("AVR Delay After Reset");
+	ui->label_6->setText("JDM Delay Cmd-to-Cmd");
+
+	ui->lineEdit_1->setToolTip("A Delay to let the board Power Up completely (milli sec)");
+	ui->lineEdit_2->setToolTip("Duration of SPI Reset pulse in milli sec");
+	ui->lineEdit_3->setToolTip("Delay for the first command after the reset pulse for SPI devices (milli sec)");
+	ui->lineEdit_4->setToolTip("Delay after reset for AT89xx devices, it increments the SPI delay (milli sec)");
+	ui->lineEdit_5->setToolTip("Delay after reset for AVR devices, it increments the SPI delay (milli sec)");
+	ui->lineEdit_6->setToolTip("Delay between two consecutive commands for JDM interface (micro sec)");
+
+	QValidator *validator = new QIntValidator(1, 999, this);
+	ui->lineEdit_1->setValidator(validator);
+	ui->lineEdit_2->setValidator(validator);
+	ui->lineEdit_3->setValidator(validator);
+	ui->lineEdit_4->setValidator(validator);
+	ui->lineEdit_5->setValidator(validator);
+	ui->lineEdit_6->setValidator(new QIntValidator(100, 10000, this));
+
+	ui->lineEdit_1->setText(QString::number(E2Profile::GetPowerUpDelay()));
+	ui->lineEdit_2->setText(QString::number(E2Profile::GetSPIResetPulse()));
+	ui->lineEdit_3->setText(QString::number(E2Profile::GetSPIDelayAfterReset()));
+	ui->lineEdit_4->setText(QString::number(E2Profile::GetAT89DelayAfterReset()));
+	ui->lineEdit_5->setText(QString::number(E2Profile::GetAVRDelayAfterReset()));
+	ui->lineEdit_6->setText(QString::number(E2Profile::GetJDMCmd2CmdDelay()));
 }
 
 PrefDialog::~PrefDialog()
@@ -79,4 +108,11 @@ void PrefDialog::on_buttonBox_accepted()
 	E2Profile::SetPICSpeed(ui->cbxBusSpeedPIC->currentIndex());
 	E2Profile::SetSDESpeed(ui->cbxBusSpeedSDE->currentIndex());
 	E2Profile::SetMicroWireSpeed(ui->cbxBusSpeedMicroWire->currentIndex());
+
+	E2Profile::SetPowerUpDelay(ui->lineEdit_1->text().toInt());
+	E2Profile::SetSPIResetPulse(ui->lineEdit_2->text().toInt());
+	E2Profile::SetSPIDelayAfterReset(ui->lineEdit_3->text().toInt());
+	E2Profile::SetAT89DelayAfterReset(ui->lineEdit_4->text().toInt());
+	E2Profile::SetAVRDelayAfterReset(ui->lineEdit_5->text().toInt());
+	E2Profile::SetJDMCmd2CmdDelay(ui->lineEdit_6->text().toUInt());
 }
