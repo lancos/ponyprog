1e88a4cb327e60d7cc6276c1fe5799079622f6c9
Claudio Lanconelli <claudiolanconelli@gmail.com>
2025-01-24 17:52:55 +0100

    Revert "moved exit code check to ponyprog.pro, where ponyprog.bat is created"
    This reverts commit bf1f480ae6c3c3a10084ab5f09f980c45b1329d7.

--- create_inno_package.bat
+++ create_inno_package.bat
@@ -9,6 +9,11 @@ call create_exe_binary.bat no_setlocal || exit /b 1
 
 mingw32-make -j%NUMBER_OF_PROCESSORS% win32setup || exit /b 1
 
-@rem ponyprog.bat is created by make win32setup
+@rem Append exit code check to commands
+echo. > build/ponyprog.bat
+for /f "delims=" %%i in (ponyprog.bat) do (
+	echo %%i ^|^| exit /b 1 >> build/ponyprog.bat
+)
+move /y build\ponyprog.bat ponyprog.bat
 
 call ponyprog.bat || exit /b 1
